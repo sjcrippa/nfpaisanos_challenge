8ce102e6fd44ef0f7a6612965907889bd2239824
Santiago Crippa <santicrippa@gmail.com>
2023-09-23 17:18:11 -0300

    adding responsive sidemenu

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -1,7 +1,13 @@
+'use client'
+import { useState } from 'react';
+import { HiOutlineMenuAlt4, HiX } from 'react-icons/hi'
+
 import Link from "next/link";
 import Image from "next/image";
 
 function Navbar() {
+  const [sideMenu, setSideMenu] = useState(false)
+
   return (
     <>
       <nav className="max-w-screen-xl flex h-20 justify-between">
@@ -14,20 +20,37 @@ function Navbar() {
             </h1>
           </div>
 
-          <div className="bg-neutral3 w-0.5 h-5 rounded-md"></div>
+          <div className="hidden lg:flex lg:bg-neutral3 w-0.5 h-5 rounded-md"></div>
 
-          <ul className="flex flex-1 gap-8 items-center align-middle">
+          <ul className="hidden lg:flex lg:flex-1 lg:gap-8 lg:items-center lg:align-middle">
             <li className="text-neutral4 text-sm font-sans font-bold"><Link href='/discover'>Discover</Link></li>
             <li className="text-neutral4 text-sm font-sans font-bold"><Link href='/about'>What We Do</Link></li>
 
           </ul>
         </div>
 
-        <div className="flex items-center ">
+        <div className="hidden lg:flex lg:items-center ">
           <button className="px-4 py-3 border-2 border-neutral4 rounded-full hover:bg-neutral8 hover:text-dark transition-all duration-200 text-neutral8 text-sm font-sans font-bold hover:border-neutral8">
             Connect Wallet
           </button>
         </div>
+
+        <button
+          className='flex lg:hidden items-center'
+          onClick={() => setSideMenu(!sideMenu)}>
+          <span className='text-neutral4 w-5 h-4'>
+            {!sideMenu ? <HiOutlineMenuAlt4 /> : <HiX />}
+          </span>
+        </button>
+
+        <nav className=
+          {`flex w-1/3 mt-16 lg:items-center sm:px-4 lg:gap-3 text-light text-md z-10 h-screen fixed transition-color duration-500 ease-in lg:hidden
+        ${!sideMenu ? '-right-[100%]' : 'fixed right-0 shadow-lg shadow-dark'}`}>
+          <ul className='flex flex-col mt-5 gap-5 mx-auto text-neutral4'>
+            <Link className='flex gap-3 text-sm font-bold border-b border-b-neutral4' href={'/discover'}>Discover</Link>
+            <Link className='flex gap-3 text-sm font-bold border-b border-b-neutral4' href={"/about"} >About</Link>
+          </ul>
+        </nav>
       </nav>
     </>
   )
